084a97607314b9a4326bb1aca60dae5385b5f291
atai <atai.abduldaev21@email.com>
2023-03-17 20:25:17 +0600

    Cr seven

---
+++ getsam.js
@@ -0,0 +1,7 @@
+function getSum(prices, discount) {
+
+}
+
+console.log(getSum([100, 200, 300], 30)) // 420 
+console.log(getSum([150, 250, 150], 10)) // 495 
+console.log(getSum([1000], 50)) // 500 
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-console.log('main');
- fix/conflict
-console.log('fix/conflict');
-console.log('console from main');
+// console.log('main');
+//  fix/conflict
+// console.log('fix/conflict');
+// console.log('console from main');
 
