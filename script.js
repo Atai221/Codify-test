000d6ee1635dba33a557072e3621191ac42e1d5b
atai <atai.abduldaev21@email.com>
2023-03-17 20:10:07 +0600

    added practice

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-console.log('main');
- fix/conflict
-console.log('fix/conflict');
-console.log('console from main');
+// console.log('main');
+//  fix/conflict
+// console.log('fix/conflict');
+// console.log('console from main');
 
+function isBefore(target, base) {
+   const targetDate=new Date(target)
+   const baseDate=new Date(base)
+   return  target>base
+        
+    
+}
+
+
+console.log(isBefore('01.01.2023', '01.02.2022')) // false
+console.log(isBefore('01.02.2022', '01.01.2023'))// true
+console.log(isBefore('01.02.2022', '01.02.2022')) // false
